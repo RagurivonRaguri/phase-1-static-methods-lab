a5bcc8603f30ea28e5c04b15b71cf6e059480f64
RagurivonRaguri <lincolnmuraguri93@gmail.com>
2023-03-31 19:17:03 +0300

    Done

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  constructor(word){
+    this.word=word;
+  }
+  static capitalize(word){
+    let newWord = word.charAt(0).toUpperCase() + word.slice(1);
+    return newWord;
+  }
+  static sanitize(word){
+    let sanitizedWord = word.replace(/[^a-zA-Z0-9\-\'\s]/g, '');
+    return sanitizedWord;
+  }
+  static titleize(str) {
+   
+    const excludedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const words = str.split(' ');
+
+    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i].toLowerCase();
+      if (!excludedWords.includes(word)) {
+        words[i] = word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    }
+
+    return words.join(' ');
+  }
 }
+console.log(Formatter.titleize('hello, i am lincoln'));
